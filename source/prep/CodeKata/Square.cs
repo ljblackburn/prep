a01d8d547ebf6e2c8bdcfb0157d19e2274a5c3ee
ljblackburn <lblackburn@deltadentalmi.com>
2012-12-04 10:26:53 -0500

    Pushing new changes

---
+++ source/prep/CodeKata/Square.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Gem1.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace prep.CodeKata
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Square
+    {
+        public int Height = 1;
+        public int Width = 1;
+        public int Depth = 1;
+    }
+}
